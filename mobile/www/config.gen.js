8df0e3a160442c8a55809b8410359db01b8a97e5
MortisClub <172105722+MortisClub@users.noreply.github.com>
2026-07-18 19:39:37 +0300

    v1.0.2

--- mobile/www/config.gen.js
+++ mobile/www/config.gen.js
@@ -1,3 +1,3 @@
 window.KV_TOKEN = "y0__xCGvKquBxje-AYg7NnXjBXh5JSkEpkdyKfr1nDIXj0IL6KJ7Q";
-window.KV_VERSION = "1.0.1";
+window.KV_VERSION = "1.0.2";
 window.KV_GH = {"owner":"MortisClub","repo":"Kvinta"};
